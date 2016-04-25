2c2d80a1fbeda9bcd9c36a98bd75fb4f48b248cc
Taras Pashko <taras.pashko@gmail.com>
2016-04-25 13:49:25 +0000

    e2

--- js/qequation.js
+++ js/qequation.js
@@ -2,9 +2,9 @@ alert ('We will solve quadratic equation: Ax2+Bx+c=0');
 math();
 function math(a, b, c) {
 var x1, x2;
-var a = parseInt(prompt ('Please enter A', ''), 10);
-var b = parseInt(prompt ('Please enter B', ''), 10);
-var c = parseInt(prompt ('Please enter C', ''), 10);
+var a = Number(prompt ('Please enter A', ''), 10);
+var b = Number(prompt ('Please enter B', ''), 10);
+var c = Number(prompt ('Please enter C', ''), 10);
 var d = b * b - ( 4 * a * c );
     if ( d > 0 ) {
         x1 = ( -b + Math.sqrt(d) ) / ( 2 * a );
